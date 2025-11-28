1e4f3a8eea9548a2b1a432d8939c4f8ec3b78cf9
Fran <mfranfc@gmail.com>
2025-11-27 23:38:15 -0500

    Update

--- statements/commands/and-r-not.sql
+++ statements/commands/and-r-not.sql
@@ -1,1 +1,5 @@
 SELECT * FROM users WHERE NOT email='mfranfc@gmail.com';
+
+SELECT * FROM users WHERE email='mfranfc@gmail.com' OR age = 20;
+
+SELECT * FROM users WHERE email='mfranfc@gmail.com' AND age = 20
